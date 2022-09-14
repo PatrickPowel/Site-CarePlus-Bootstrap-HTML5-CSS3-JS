4bd899ccc83a4ecf671183ba0f6e1a92f9d01c12
Patrick Willians Powel <61129859+PatrickPowel@users.noreply.github.com>
2022-09-14 20:02:14 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+jQuery(document).ready(function(){
+
+
+
+
+
+ /***************** jquery scrool spy ******************/
+
+
+ /***************** prallax with scrolly ******************/
+
+
+ /***************** bootstrap mobile menu coustomized ******************/
+
+
+
+
+
+});
+
+
+
+jQuery(window).scroll(function () {
+
+
+});
+
+
+jQuery(window).load(function($){
+	    
+
+
+});
